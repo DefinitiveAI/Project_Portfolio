87a911959e66eb29407ca27b608a4a6f62469da2
DefinitiveAI <dan@heinen-online.de>
2025-04-26 17:36:38 +0100

    Update type_annotations.py
    Added necessary type annotations

--- src/streaming_model_experimentation/type_annotations.py
+++ src/streaming_model_experimentation/type_annotations.py
@@ -1,1 +1,7 @@
+# from beartype.vale import Is  # <---- validator factory
+# from typing import Annotated  # <---------------- if Python ≥ 3.9.0
+from river.base import Dataset, Metric, Estimator
+from expectation.modules.hypothesistesting import EProcess
 
+Reals = Union[int, float]
+__all__ = ["Dataset", "Metric", "Estimator", "Reals", "EProcess"]
