a2a1716ac4ea933451da901f11ad2235f176cfc3
DefinitiveAI <dan@heinen-online.de>
2025-04-26 18:57:20 +0100

    Update type_annotations.py

--- src/streaming_model_experimentation/type_annotations.py
+++ src/streaming_model_experimentation/type_annotations.py
@@ -3,5 +3,5 @@
 from river.base import Dataset, Metric, Estimator
 # from expectation.seqtest.sequential_e_testing import SequentialTest
 
-Reals = Union[int, float]
-__all__ = ["Dataset", "Metric", "Estimator", "Reals"]
+# Reals = Union[int, float]
+__all__ = ["Dataset", "Metric", "Estimator"]
