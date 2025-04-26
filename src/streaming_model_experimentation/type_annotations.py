82ede89f5fa38b68a30b4f64e2459182edaa19d2
DefinitiveAI <dan@heinen-online.de>
2025-04-26 18:45:01 +0100

    Update type_annotations.py

--- src/streaming_model_experimentation/type_annotations.py
+++ src/streaming_model_experimentation/type_annotations.py
@@ -1,7 +1,7 @@
 # from beartype.vale import Is  # <---- validator factory
 # from typing import Annotated  # <---------------- if Python ≥ 3.9.0
 from river.base import Dataset, Metric, Estimator
-from expectation.modules.hypothesistesting import EProcess
+# from expectation.seqtest.sequential_e_testing import SequentialTest
 
 Reals = Union[int, float]
-__all__ = ["Dataset", "Metric", "Estimator", "Reals", "EProcess"]
+__all__ = ["Dataset", "Metric", "Estimator", "Reals"]
